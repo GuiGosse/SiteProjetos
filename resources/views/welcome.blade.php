7f947428fae7e1255d4daa653304124511240457
guigosse <guilhermegosse@gmail.com>
2025-02-26 22:10:29 -0300

    Subindo arquivos novamente

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha Página</title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+            width: 100%;
+            margin: 0;
+            background-color: #f4f4f4;
+        }
+        img {
+            max-width: 100%;
+            height: auto;
+        }
+    </style>
+</head>
+<body>
+    <img src="https://guilhermegosse.onrender.com/storage/imagens/anuncie-aqui.png" alt="Anuncie Aqui">
+</body>
+</html>
